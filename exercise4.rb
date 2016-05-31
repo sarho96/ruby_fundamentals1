ea9f67a612890def4eaa0ab0c9a2c279dcb72f5f
Stella <stellaha4@gmail.com>
2016-05-31 14:35:30 -0400

    created Bitmaker Labs version of FizzBuzz

---
+++ exercise4.rb
@@ -0,0 +1,12 @@
+num = 1
+while num <= 100 do
+  if num%3==0 && num%5==0 then
+    puts "BitMaker"
+  elsif num%3==0
+    puts "Bit"
+  elsif num%5==0
+    puts "Maker"
+  end
+  puts num
+  num += 1
+end
