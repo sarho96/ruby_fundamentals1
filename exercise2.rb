37f54b42e2f7a0d2749c25a50716fb441e321185
Stella <stellaha4@gmail.com>
2016-05-31 13:53:52 -0400

    created program to show good tip

---
+++ exercise2.rb
@@ -0,0 +1,4 @@
+meal_cost = 55 #declare variable called meal_cost and assign value 55
+tax_percentage = 0.15 # declare variable caled tax_percentage and assign value 0.13 for tax rate
+good_tip = meal_cost*tax_percentage #declare variable good_tip and assign its value as the result of 55*0.13
+puts "A good tip will be 15% of the total meal cost. I will pay #{good_tip} for a 55 dollar meal"
