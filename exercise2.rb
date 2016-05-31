3cf4d253360709fad99638304d771ecc6a7c3984
Stella <stellaha4@gmail.com>
2016-05-31 14:09:37 -0400

    added a new string that adds an integer and a string

--- exercise2.rb
+++ exercise2.rb
@@ -2,3 +2,8 @@ meal_cost = 55 #declare variable called meal_cost and assign value 55
 tax_percentage = 0.15 # declare variable caled tax_percentage and assign value 0.13 for tax rate
 good_tip = meal_cost*tax_percentage #declare variable good_tip and assign its value as the result of 55*0.13
 puts "A good tip will be 15% of the total meal cost. I will pay #{good_tip} for a 55 dollar meal"
+
+
+num1 = "1" #declared variable num1 and assigned value "1"
+num2 = 2 #declared variable num2 and assigned value 2
+puts num1.to_i + num2 #output on console num1 + num2 with num1 converting to integer value
